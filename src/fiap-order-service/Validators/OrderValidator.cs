780b1bd36b97b7d91db22142230327fce9b1277b
Guilherme Felipe Martins <guilherme.martins92@outlook.com>
2025-05-23 20:51:28 -0300

    Add validation for Order model and update project dependencies
    Updated `fiap-order-service.csproj` to include new package references for `FluentValidation`, `Microsoft.AspNetCore.OpenApi`, and `Swashbuckle.AspNetCore`. Removed the folder reference for validators.
    
    Added `OrderValidator.cs` to implement validation rules for the `Order` model, ensuring required fields and proper formats for `CustomerDocument`, `CustomerName`, `CustomerEmail`, and `Itens`.

---
+++ src/fiap-order-service/Validators/OrderValidator.cs
@@ -0,0 +1,23 @@
+﻿using fiap_order_service.Models;
+using FluentValidation;
+
+namespace fiap_order_service.Validators
+{
+    public class OrderValidator : AbstractValidator<Order>
+    {
+        public OrderValidator()
+        {
+            RuleFor(x => x.CustomerDocument)
+                .NotEmpty().WithMessage("Customer document is required.")
+                .Length(11).WithMessage("Customer document must be 11 characters long.");
+            RuleFor(x => x.CustomerName)
+                .NotEmpty().WithMessage("Customer name is required.")
+                .MaximumLength(100).WithMessage("Customer name must not exceed 100 characters.");
+            RuleFor(x => x.CustomerEmail)
+                .NotEmpty().WithMessage("Customer email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+            RuleFor(x => x.Itens)
+                .NotEmpty().WithMessage("At least one item is required.");
+        }
+    }
+}
